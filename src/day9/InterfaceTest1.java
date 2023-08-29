f751313df3369b178010340757f733d43db792db
HaInseong <gkdlstjd1020@naver.com>
2023-08-29 15:50:04 +0900

    인터페이스에 대한 학습 및 주석으로 정리

--- src/day9/InterfaceTest1.java
+++ src/day9/InterfaceTest1.java
@@ -1,6 +1,7 @@
 package day9;
 
 interface Printable {
+	//public static final, public abstract가 자동으로 추가되니 빼고 개발하는 코드도 많다.
 	public static final int PRINT_TYPE1 = 1;
 	static final int PRINT_TYPE2 = 2;
 	final int PRINT_TYPE3 = 3;
@@ -8,8 +9,9 @@ interface Printable {
 
 	public abstract void print(int type);
 	abstract void setPage(int su);
-	boolean isAvailable();
+	boolean isAvailable(); //public abstract 생략
 }
+
 
 abstract class Shape3 {
 	String color;
@@ -20,6 +22,7 @@ abstract class Shape3 {
 		this.color = color;
 	}
 }
+
 
 class Circle3 extends Shape3 implements Printable {
 	int page;
@@ -44,6 +47,7 @@ class Circle3 extends Shape3 implements Printable {
 	}
 }
 
+
 class Rectangle3 extends Shape3 {
 	void draw() {
 		System.out.println(color + " 사각형을 그리는 기능");
@@ -56,13 +60,14 @@ class Triangle3 extends Shape3 {
 	}
 }
 
+
 public class InterfaceTest1 {
 	public static void main(String args[]) {
 		printOut(new Circle3());
 		//printOut(new Rectangle3()); // 컴파일 오류가 발생하는 행
 	}
 
-	static void printOut(Printable prt) {
+	static void printOut(Printable prt) { //Printable을 추가상속(인터페이스 구현)하고 있는 객체만 매개변수로 받는다. Rectangle3는 매개변수로 올수없음.
 		prt.setPage(100);
 		((Shape3) prt).setColor("파란색");
 		if (prt.isAvailable())
