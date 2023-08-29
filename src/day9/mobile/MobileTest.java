60cb08e359a6fe8549e76b1cb03ea080e702be6a
HaInseong <gkdlstjd1020@naver.com>
2023-08-29 14:25:14 +0900

    참조변수 자바 형식에 맞게 소문자로 변경

--- src/day9/mobile/MobileTest.java
+++ src/day9/mobile/MobileTest.java
@@ -4,34 +4,34 @@ public class MobileTest {
 
 	public static void main(String[] args) {
 		
-		Mobile Ltab = new Ltab("Ltab", 500, "ABC-01");
-		Mobile Otab = new Otab("Otab", 1000, "XYZ-20");
+		Mobile ltab = new Ltab("Ltab", 500, "ABC-01");
+		Mobile otab = new Otab("Otab", 1000, "XYZ-20");
 		
 		//생성된 객체 정보 출력
 		MobileTest.printTile();
 		System.out.println("-----------------------------------------");
-		MobileTest.printMobile(Ltab);
-		MobileTest.printMobile(Otab);
+		MobileTest.printMobile(ltab);
+		MobileTest.printMobile(otab);
 		
 		System.out.println();
 		
 		System.out.println("[10분 충전]");
 		System.out.println("-----------------------------------------");
 		MobileTest.printTile();
-		Ltab.charge(10);
-		Otab.charge(10);
-		MobileTest.printMobile(Ltab);
-		MobileTest.printMobile(Otab);
+		ltab.charge(10);
+		otab.charge(10);
+		MobileTest.printMobile(ltab);
+		MobileTest.printMobile(otab);
 		
 		System.out.println();
 		
 		System.out.println("[5분 통화]");
 		System.out.println("-----------------------------------------");
 		MobileTest.printTile();
-		Ltab.operate(5);
-		Otab.operate(5);
-		MobileTest.printMobile(Ltab);
-		MobileTest.printMobile(Otab);
+		ltab.operate(5);
+		otab.operate(5);
+		MobileTest.printMobile(ltab);
+		MobileTest.printMobile(otab);
 
 
 	}
