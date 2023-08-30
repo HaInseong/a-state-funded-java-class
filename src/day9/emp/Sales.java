b52193327e7262fa9ee37114d1ce1376a16b4e42
HaInseong <gkdlstjd1020@naver.com>
2023-08-30 09:56:42 +0900

    실습문제 피드백 주신거 주석에 반영

--- src/day9/emp/Sales.java
+++ src/day9/emp/Sales.java
@@ -4,6 +4,7 @@ public class Sales extends Employee implements Bonus {
 	
 	public Sales() {}
 	public Sales(String name, int number, String department, int salary) {
+		//전부 조상에게 보내서 조상 필드에 세팅해야 함
 		super(name, number, department, salary);
 	}
 	
@@ -16,6 +17,7 @@ public class Sales extends Employee implements Bonus {
 		super.setSalary(super.getSalary() + (int)(pay * 1.2));
 	}
 	
+	//추가 메서드
 	public double getExtraPay() {
 		return super.getSalary() * 0.03;
 	}
