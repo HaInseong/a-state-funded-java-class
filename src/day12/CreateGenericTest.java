e97a7171bed57c4c9a80cb710700aff5cc101095
HaInseong <gkdlstjd1020@naver.com>
2023-09-01 10:52:37 +0900

    제네릭 학습

--- src/day12/CreateGenericTest.java
+++ src/day12/CreateGenericTest.java
@@ -27,25 +27,29 @@ public class CreateGenericTest {
 	}
 }
 
-class Value1 {
+class Value1 { //String형에 특화된 클래스
 	String obj;
-	void put(String obj) {
+	void put(String obj) { //obj의 값을 초기화해주는 메서드
 		this.obj = obj;		
 	}
 	String get() {
 		return obj;
 	}
 }
-class Value2 {
+class Value2 { //Object의 다형성 측면에서 모든 자료형에 특화된 클래스
 	Object obj;
 	void put(Object obj) {
 		this.obj = obj;		
 	}
 	Object get() {
-		return obj;
+		return obj; //Date형을 가져온다면 get()는 Object형으로 반환하기 때문에 Date형으로 다시 변환해서 사용해야한다.
 	}
 }
-class Value3<T> {
+//제네릭을 이용하면 Object형보다 다양한 객체에 대한 처리를 세련되게 할 수 있다.
+//Value3를 객체생성하는데 어떤 객체를 다룰 것인지 Value3 객체를 생성하는 시점에서 타입을 전달한다는 의미이다.
+// Value3<Date> v = new Value3<Date>(); Value3 객체를 생성하면 T가 모두 Date로 바뀐다.
+// Value3<Friend> v = new Value3<Friend>(); T가 모두 Friend로 바뀐다. 클래스를 만드는 시점이 아니라 클래스를 사용하는 시점에서 데이터의 타입을 결정한다.
+class Value3<T> { //<T> = Type parameter라 한다.
 	T obj;
 	void put(T obj) {
 		this.obj = obj;		
