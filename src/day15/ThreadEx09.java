1712ac5e5b5093b2d6b960924a641716c4f8c530
HaInseong <gkdlstjd1020@naver.com>
2023-09-06 17:42:57 +0900

    스레드 학습

--- src/day15/ThreadEx09.java
+++ src/day15/ThreadEx09.java
@@ -19,6 +19,7 @@ public class ThreadEx09 {
 		threadA.join(); //해당 스레드 객체의 기능이 끝날 때 까지 메인스레드는 일시정지 하는 기능
 		threadB.join(); //쓰레드 b라는 객체로 기동된 스레드가 끝날 때 까지 메인 스레드는 대기해라.
 		threadC.join(); //자식 스레드 수행이 끝날 때 까지 기다렸다가 메인 스레드를 실행할 때 사용하는 메서드임
+		//3개의 워커스레드를 다 실행하고 메인 스레드를 실행하겠다는 의미이다.
 
 		for (int i = 0; i < 3; i++)
 			System.out.println("프로그램 시작 스레드 이름: " + mainThread.getName());
